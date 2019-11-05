1d85556ef854a8fee16a938e782040a69d06c52e
Ricardo Manoel <rdomanoel@gmail.com>
2019-11-04 23:46:18 -0300

    dev: button react example

---
+++ react-jsx/src/Button.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Button = () => {
+
+  return (
+    <button
+      className="App-button"
+      onClick={() => alert('The button was clicked!')}
+    >Click here!</button>
+  )
+};
+
+export default Button;
